d58f4b7c9db6ae5a9d9e06f808f517d335d7dbe7
vincesesto <vince.sesto@foodstuffs.co.nz>
2018-09-17 16:57:07 +1200

    Adding in rules and ansible lint

---
+++ chapter7/test_rules/LineLength.py
@@ -0,0 +1,14 @@
+from ansiblelint import AnsibleLintRule
+
+
+class LineLength(AnsibleLintRule):
+    id = 'ANSWERS01'
+    shortdesc = 'Line too long'
+    description = 'Python Code Style Guidelines Recommend Line Lenghth Under 80 Characters'
+    tags = ['formatting']
+
+    def match(self, file, line):
+        if len(line) > 80:
+            self.shortdesc += " ({} characters)".format(len(line))
+            return True
+        return False
